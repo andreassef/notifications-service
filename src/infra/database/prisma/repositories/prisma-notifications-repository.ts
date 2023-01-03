f52b9a5e1d3504cbd41c180ab965f9cca23872b8
assef <andreassef02@gmail.com>
2023-01-03 00:20:03 -0300

    [ UPDATE ] - adding methods in module because of implementation interface

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -8,6 +8,10 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 export class PrismaNotificationRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const rawData = PrismaNotificationMapper.toPrisma(notification);
     await this.prismaService.notification.create({
@@ -15,7 +19,7 @@ export class PrismaNotificationRepository implements NotificationsRepository {
     });
   }
 
-  findById(notificationId: string): Promise<Notification | null> {
+  async findById(notificationId: string): Promise<Notification | null> {
     throw new Error('Method not implemented.');
   }
 }
