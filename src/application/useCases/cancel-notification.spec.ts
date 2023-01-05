e9865c5c8d7dd49fc423e9d7cee314e4c2e86585
assef <andreassef02@gmail.com>
2023-01-04 23:52:33 -0300

    [ UPDATE ] - cancel a notification

--- src/application/useCases/cancel-notification.spec.ts
+++ src/application/useCases/cancel-notification.spec.ts
@@ -2,9 +2,10 @@ import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel notification', () => {
-  it('should be able to send a notification', async () => {
+  it('should be able to cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
@@ -21,5 +22,16 @@ describe('Cancel notification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('should to be able to cancel a notification when it does not exist', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake_notification_id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
