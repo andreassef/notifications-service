1b4f2b86375acc32372e886be54db5df9dadfb1c
assef <andreassef02@gmail.com>
2023-01-08 00:33:25 -0300

    [  UPDATE ] - Importing use case modules

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from '@application/useCases/cancel-notification';
+import { CountRecipientNotification } from '@application/useCases/count-recipient-notification';
+import { GetRecipientNotification } from '@application/useCases/get-recipient-notification';
+import { ReadNotification } from '@application/useCases/read-notification';
+import { UnreadNotification } from '@application/useCases/unread-notification';
 import { Module } from '@nestjs/common';
 import { SendNotification } from 'src/application/useCases/send-notification';
 import { DatabaseModule } from '../database/database.module';
@@ -6,6 +11,13 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
